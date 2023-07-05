75e22124023e63410015486c25f5f91595b40f12
kausalya prusty <128567862+kaushalya-prusty@users.noreply.github.com>
2023-07-05 13:06:17 +0530

    character counter

---
+++ Character counter/index.js
@@ -0,0 +1,12 @@
+const textareaEl = document.getElementById("textarea");
+const totalEl = document.getElementById("total");
+const remainingEl = document.getElementById("remaining");
+textareaEl.addEventListener("keyup", () => {
+  updateCounter();
+});
+updateCounter();
+function updateCounter() {
+  totalEl.innerText = textareaEl.value.length;
+  remainingEl.innerText =
+    textareaEl.getAttribute("maxLength") - textareaEl.Value.length;
+}
